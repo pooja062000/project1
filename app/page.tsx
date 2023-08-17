9aec1eafcfd9cecbccb8ccd3f47a5cc79907d9e5
pooja062000 <vikaskosli9890@gmail.com>
2023-08-17 16:20:17 +0530

    new changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,9 @@
 import Image from 'next/image'
-import styles from './page.module.css'
-import Link from 'next/link'
+
 export default function Home() {
   return (
-    <main className={styles.main}>
+   <>
    
-<Link href='/home'> Home </Link>
-    <Link href='about'> About </Link> 
-    <Link href='services'> Services </Link> 
-    <Link href='contact'> Contact </Link> 
-  
-    </main>
-  
+   </>
   )
 }
